af3a5acd75cf9bb837304d4494a2927725782d24
looneylu <lcarbuccia@gmail.com>
2014-07-31 20:18:23 -0400

    Initial Commit

---
+++ AThousandWords/AThousandWords/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AThousandWords
+//
+//  Created by Luis Carbuccia on 7/31/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
