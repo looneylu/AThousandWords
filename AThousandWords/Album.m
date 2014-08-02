e915a65b7840949e4c39956b149b96265534acaf
looneylu <lcarbuccia@gmail.com>
2014-08-01 22:09:46 -0400

    added album.h (NSManagedObject)

---
+++ AThousandWords/Album.m
@@ -0,0 +1,17 @@
+//
+//  Album.m
+//  AThousandWords
+//
+//  Created by Luis Carbuccia on 8/1/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import "Album.h"
+
+
+@implementation Album
+
+@dynamic name;
+@dynamic date;
+
+@end
